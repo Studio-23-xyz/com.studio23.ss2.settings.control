44deda1544799ebcf218bf3df50a832363ea23a3
dipokDipu <90243333+dipokDipu@users.noreply.github.com>
2024-04-09 00:12:25 +0600

    Changed Intialize funtion and added data class constructor

--- Assets/Packages/com.studio23.ss2.settings.control/Runtime/Data/ControlSettingsConfiguration.cs
+++ Assets/Packages/com.studio23.ss2.settings.control/Runtime/Data/ControlSettingsConfiguration.cs
@@ -1,15 +1,34 @@
 
+using FlyingWormConsole3;
 using UnityEngine;
 
 namespace  Studio23.SS2.Settings.Control.Data
 {
     [CreateAssetMenu(fileName = "ControlSettingsConfiguration", menuName = "Studio-23/Settings/Control/ControlSettingsConfig", order = 1)]
+    [System.Serializable]
     public class ControlSettingsConfiguration : ScriptableObject
     {
-        public int InvertXAtStart;
-        public int InvertYAtStart;
-        public float MouseSensitivityAtStart;
-        public float ControllerSensitivityAtStart;
+        public int IsInvertX;
+        public int IsInvertY;
+        public float MouseSensitivity;
+        public float ControllerSensitivity;
+
+        public ControlSettingsConfiguration(int invertX, int invertY, float mouseSen, float controllerSen)
+        {
+            IsInvertX= invertX;
+            IsInvertY= invertY;
+            MouseSensitivity= mouseSen;
+            ControllerSensitivity= controllerSen;
+        }
+
+        public ControlSettingsConfiguration(ControlSettingsConfiguration data)
+        {
+            IsInvertX= data.IsInvertX;
+            IsInvertY= data.IsInvertY;
+            MouseSensitivity= data.MouseSensitivity;
+            ControllerSensitivity= data.ControllerSensitivity;
+        }
+
     }
 
 }
